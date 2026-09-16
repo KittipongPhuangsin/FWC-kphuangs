613a1f1a405dbd173f51b516ae96d5fec17cb3fd
KittipongPhuangsin <66070016@kmitl.ac.th>
2026-09-16 21:13:55 +0700

    add cell02 ex03 and cell03 ex00 ex01

---
+++ Cell03/ex01/balloon.js
@@ -0,0 +1,37 @@
+let size = 200;
+let color = 0;
+
+let colors = ["red", "green", "blue"];
+
+function growBalloon() {
+    size = size + 10;
+    color = color + 1;
+
+    if (color > 2) {
+        color = 0;
+    }
+
+    if (size > 420) {
+        size = 200;
+    }
+
+    document.getElementById("balloon").style.width = size + "px";
+    document.getElementById("balloon").style.height = size + "px";
+    document.getElementById("balloon").style.backgroundColor = colors[color];
+}
+
+function shrinkBalloon() {
+    if (size > 200) {
+        size = size - 5;
+    }
+
+    color = color - 1;
+
+    if (color < 0) {
+        color = 2;
+    }
+
+    document.getElementById("balloon").style.width = size + "px";
+    document.getElementById("balloon").style.height = size + "px";
+    document.getElementById("balloon").style.backgroundColor = colors[color];
+}
